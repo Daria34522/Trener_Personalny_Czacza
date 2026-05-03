c89d16fbf3b41d8bd46560b714fa009acff95988
Daria <daria34522@gmail.com>
2026-05-03 18:50:23 +0200

    Komentarze

--- coach/voice_control/voice_control.py
+++ coach/voice_control/voice_control.py
@@ -4,12 +4,13 @@ import pygame
 import time
 import speech_recognition as sr
 
-
+# Klasa nasłuchująca co mówi użytkownik
 class Listener:
     def __init__(self, language = "pl"):
         self.language = language
         self.recognizer = sr.Recognizer()
 
+    # Zwracanie jako tekstu tego co mówi użytkownik
     def listen(self):
         with sr.Microphone() as source:
             self.recognizer.energy_threshold = 200
@@ -26,6 +27,7 @@ class Listener:
             except sr.RequestError as e:
                 return e
 
+# Obsługa mowy asystenta
 class Speaker:
     def __init__(self, lang ="pl", voice_filename= "voice.mp3"):
         self.lang = lang
@@ -34,10 +36,12 @@ class Speaker:
         self.voice_filename = os.path.join(self.folder, voice_filename + ".mp3")
         pygame.mixer.init()
 
+    # Tworzenie pliku audio
     def gen_speak(self, text):
         tts = gTTS(text=text, lang=self.lang)
         tts.save(self.voice_filename)
 
+    # Funkcja mówiąca
     def speak(self):
         pygame.mixer.music.load(self.voice_filename)
         pygame.mixer.music.play()
@@ -45,6 +49,7 @@ class Speaker:
             time.sleep(0.1)
         pygame.mixer.music.unload()
 
+    # Kasowanie pliku audio
     def delete_file(self):
         if(os.path.exists(self.voice_filename)):
             os.remove(self.voice_filename)
