6762bf238eeaeef52768fa29d2c99bed8759d304
JanuszB2004 <255557@edu.p.lodz.pl>
2026-05-06 20:44:39 +0200

    Dodanie menu wyboru profilu, menu głównego, statystyk oraz podglądu kamery (#5)
    * Dodanie menu wyboru oraz tworzenia profilu'
    
    * Dodanie menu głównego
    
    * Dodanie menu kalibracji kamer
    
    * Poprawki
    
    * Poprawki menu obsługującego kalibracje kamery oraz dodanie menu statystyk
    
    * Reogranizacja wyboru profilu
    
    * Modyfikacja menu wyboru profilu poprzez dodanie obsługi zdjęć profilowych
    
    * Dodanie Ikon
    
    ---------
    
    Co-authored-by: MichalKawecki727 <255621@edu.p.lodz.pl>

---
+++ coach/interface/Stats.py
@@ -0,0 +1,49 @@
+import sys
+from sys import path
+
+from PySide6.QtMultimedia import QCamera, QMediaCaptureSession
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+
+
+class Stats(QMainWindow):
+    def __init__(self):
+        # Ładowanie pliku .ui
+        super().__init__()
+        ui_path = "ui/Stats_menu.ui"
+        loader = QUiLoader()
+        ui_file = QFile(ui_path)
+        if not ui_file.open(QFile.ReadOnly):
+            print(f"Nie można otworzyć pliku: {ui_path}")
+            sys.exit(-1)
+        self.ui = loader.load(ui_file, self)
+        ui_file.close()
+        self.setWindowTitle("Statystyki")
+
+        # TODO Wpisanie nazwy użytkownika w pole 'Zalogowany użytkownik:'
+
+        # Łączenie przycisków z metodami
+        self.ui.Draw_graph.clicked.connect(self.drawGraph)
+        self.ui.Selected_date.selectionChanged.connect(self.displayDayStats)
+
+    # Daty pobrane z ui zapisane są w tablicy posiadającej 3 elementy typu String tj. dzień, miesiąc, rok
+    def drawGraph(self):
+        first_date = self.ui.From_date.text().split(".")
+        second_date = self.ui.Until_date.text().split(".")
+        # TODO pobranie danych z bazy danych oraz narysowanie wykresu
+        pass
+
+    def displayDayStats(self):
+        date = self.ui.Selected_date.selectedDate().toString("dd-MM-yyyy").split("-")
+        # TODO pobranie danych z bazy danych oraz wyświetlenie danych
+        pass
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = Stats()
+    window.show()
+
+    sys.exit(app.exec())
